8a1fb52be645c0a0a2c70ddf469a58b2d29a211f
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-01-29 22:44:31 +0545

    if-leaves-chat

--- Lightchatapp/chat/src/main/java/com/webchat/chat/config/WebSocketeventListener.java
+++ Lightchatapp/chat/src/main/java/com/webchat/chat/config/WebSocketeventListener.java
@@ -1,9 +1,13 @@
 package com.webchat.chat.config;
 
 
+import com.webchat.chat.chat.ChatMessage;
+import com.webchat.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -12,9 +16,22 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketeventListener {
 
+
+    private final SimpMessageSendingOperations messageTemplate;
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
 //        Here it is to notify if any user leaves the chatbox
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+//            Here the topic/public means the chat will be visible to everyone inside this platform;
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
 
     }
 }
